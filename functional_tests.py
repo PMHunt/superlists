d3a3a5989ceef0bd4ccb06675dc6747f3eac4010
Phil Hunt <phil@deviantlogic.co.uk>
2017-07-20 15:45:11 +0100

    refactored FT with helper method

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    # helper to enable refactoring
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith requests our homepage (locally for now)
@@ -34,11 +41,18 @@ class NewVisitorTest(unittest.TestCase):
         # '1: Buy peacock feathers' is now a line item in the list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        #there is still a text box allowing her to add more to-dos
+        # she enters 'Use peacock feathers to make a fly'
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #the page updates again and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # There is still a text box inviting more to-dos
         self.fail('finish the test')
